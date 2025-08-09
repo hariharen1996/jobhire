7381236390b468a0f662075c6f460a669f9023d1
hariharen96 <hariharen1996@gmail.com>
2025-08-09 18:48:35 +0530

    feat: create users app and add Django REST Framework Created 'users' app for authentication and added it to INSTALLED_APPS Installed Django REST Framework and added 'rest_framework' and 'rest_framework.authtoken' to INSTALLED_APPS Updated requirements.txt accordingly

--- jobhire/settings.py
+++ jobhire/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
