154c1950bd3145a72f6da2be3e659c574e391c32
hariharen96 <hariharen1996@gmail.com>
2025-08-13 11:36:31 +0530

    feat: Add authenticated home view and include jobs URLs Implemented a protected home endpoint that returns a welcome message and serialized user data Applied IsAuthenticated permission to restrict access Registered jobs app URLs using path('api/jobs/', include('jobs.urls'))

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from users.serializers import  UserSerializer
+
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def home(request):
+    user = request.user 
+    data = {
+        "message": f"welcome to jobhire, {user.username}",
+        'user': UserSerializer(user).data
+    }
+    return Response(data,status=status.HTTP_200_OK)
