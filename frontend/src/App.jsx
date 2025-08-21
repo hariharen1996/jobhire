c15ee08bac840460da65563cd1e2d432659abea7
hariharen96 <hariharen1996@gmail.com>
2025-08-21 10:43:25 +0530

    feat: implement HomePage with Tailwind CSS and routing Implement HomePage with Create Profile, Dashboard, View Profile buttons Add routing using Link for Home button in Navbar Add CSS styles and Tailwind for HomePage UI Add BrowserRouter in main.jsx and routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home/HomePage'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar-bg flex flex-col md:flex-row md:justify-between items-center gap-2 md:gap-0 px-3 py-2">
@@ -5,14 +7,14 @@ const Navbar = () => {
         <h1 className="text-white">Jobhire</h1>
       </div>
       <div className="flex gap-2">
+        <Link to="/">
           <button className="flex items-center gap-2 px-2 py-1 rounded text-white cursor-pointer home-btn transition hover:bg-blue-600 border-1 border-blue-500">
             <i className="fas fa-laptop-house"></i> Home
           </button>
-          <button
-            className="flex items-center gap-2 px-2 py-1 rounded text-white cursor-pointer home-btn transition hover:bg-blue-600 border-1 border-blue-500"
-          >
-            <i className="fas fa-sign-out-alt"></i> Logout
-          </button>
+        </Link>
+        <button className="flex items-center gap-2 px-2 py-1 rounded text-white cursor-pointer home-btn transition hover:bg-blue-600 border-1 border-blue-500">
+          <i className="fas fa-sign-out-alt"></i> Logout
+        </button>
       </div>
     </nav>
   );
